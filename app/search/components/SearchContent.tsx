61acd392d7bfd075c958f65c047440713b3ca326
Igor Shchetinkin <fastigor293015@gmail.com>
2023-06-20 15:18:41 +1000

    removed unnecessary imports in SearchContent and QueueContent

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
